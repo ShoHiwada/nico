f1a0938fff0d33506b2647292b2d4134f7585809
sho-hiwada <hiwasho46@gmail.com>
2025-05-29 15:29:57 +0900

    chore: @vitejs/plugin-vue追加＋vite.config.js調整
    - plugin-vue を devDependencies に追加
    - vite.config.js に vue() を設定
    - baseやHMRの開発用設定を明確化

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/',
+    plugins: [
+        vue(),
+        laravel({
+          input: ['resources/css/app.css', 'resources/js/spa.js'],
+          refresh: true,
+      }),
+    ],
     server: {
         host: '0.0.0.0',
         hmr: {
             host: 'localhost'
-        }
+        },
     },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
 });
